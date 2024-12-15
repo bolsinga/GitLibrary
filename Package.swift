114cf0ee1f0ddb5ecc49f2588248ced42ef48022
Greg Bolsinga <bolsinga@mac.com>
2024-12-15 15:33:05 -0800

    format

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "GitLibrary",
-    platforms: [ .macOS(.v15) ],
-    products: [ .library(name: "GitLibrary", targets: ["GitLibrary"]) ],
-    targets: [ .target( name: "GitLibrary") ]
+  name: "GitLibrary",
+  platforms: [.macOS(.v15)],
+  products: [.library(name: "GitLibrary", targets: ["GitLibrary"])],
+  targets: [.target(name: "GitLibrary")]
 )
